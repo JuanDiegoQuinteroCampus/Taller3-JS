f2cdb639c328fd50a9c2ea944283aaedb59435ba
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-03-15 09:24:19 -0500

    feat: :sparkles: Taller3-JS
    Se inicio un repocitorio acerca del taller 3, en proceso el punto 1

---
+++ main.js
@@ -0,0 +1,48 @@
+/* 1. Construir un objeto literal "campus" que gestione
+la info(PROPIEDADES) de Campus, trainers, campers, niveles,
+tecnologías, teams y roadMap
+
+1.1. De campus administrar los datos de contacto de las sedes en
+Bucaramanga, Bogotá, Medellín y México
+1.2. De los trainers y campers, su nombre, sus teléfonos, teams
+(horarios de las teams=> día, hora y salones (nro y piso), y el
+email, y de los campers también horarios de inglés y ser.
+1.3. De los campers, también gestionar su nivel actual, como su
+barrio y medio de transporte
+1.4. De los niveles, su pre requisito, a que tecnología pertenece, si
+es electiva u obligatoria
+1.5. De la roadmap , Nro de créditos, año, Nro de asignaturas */
+
+let myFormularioCampus = document.querySelector("#myFormularioCampus");
+let myFormularioPersonas = document.querySelector("#myFormularioPersonas");
+let campus = {};
+
+myFormularioCampus.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    campus[`${data.nombreSede}`] = {Camper: [], Trainers: []};
+    listaSedes();
+    myFormularioCampus.reset();
+})
+
+let listaSedes = ()=>{
+    let opciones = document.querySelector("[name='sede']");
+    opciones.innerHTML = null;
+    for (let [val, id] of Object.entries(campus)) {
+        opciones.insertAdjacentHTML("beforeend", `
+            <option value="${val}">${val}</option>
+        `);
+    }
+}
+
+myFormularioPersonas.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede = data.sede;
+    delete data.sede;
+    campus[`${sede}`]["Camper"].unshift(data);
+    console.log(campus);
+    myFormularioPersonas.reset();
+})
+
